2b68a08c07c45526292afdb60d5a1f898821cce9
Weikang <weikang.ng@evdg.cc>
2025-08-01 15:46:41 +0800

    Add environment variables setup and configuration

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,19 +1,19 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://localhost:3005/ecommerce',
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3005/ecommerce",
 });
 
 API.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token && !config.headers?.Authorization) {
       config.headers = config.headers || {};
-      config.headers['Authorization'] = `Bearer ${token}`;
+      config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 
 export default API;
