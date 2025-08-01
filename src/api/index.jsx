073577d9684f4d0b40d8e6f62c24156875b21c37
Weikang <weikang.ng@evdg.cc>
2025-08-01 16:04:01 +0800

    update

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3005/ecommerce",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 
 API.interceptors.request.use(
